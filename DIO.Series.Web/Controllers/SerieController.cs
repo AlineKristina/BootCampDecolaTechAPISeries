fbc54527fde8710b9d5aeb3eef61c9a97d063ad3
Aline C <aline.cristina.2k17@outlook.com>
2021-09-20 12:43:35 -0300

    API para cadastramento de séries e filmes.

---
+++ DIO.Series.Web/Controllers/SerieController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIO.Series.Web;
+
+namespace DIO.Series.Web.Controllers
+{
+    [Route("[controller]")]
+    public class SerieController : Controller
+    {
+        /*
+         * Get = Retorno
+         * Post = Inserção
+         * Put = Alteração
+         * Delete = Exclusão
+         */
+
+        private readonly IRepositorio<Serie> _repositorioSerie;
+
+        public SerieController(IRepositorio<Serie> repositorioSerie)
+        {
+            // Injeção de dependencia: tira do controler a responsabilidade de conhecer a classe concreta
+            _repositorioSerie = repositorioSerie;
+        }
+
+        [HttpGet("")]
+        public IActionResult Lista()
+        {
+            return Ok(_repositorioSerie.Lista().Select(s => new SerieModel(s)));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualiza(int id, [FromBody] SerieModel model)
+        {
+            _repositorioSerie.Atualiza(id, model.ToSerie()); //AutoMapper
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            _repositorioSerie.Exclui(id);
+            return NoContent();
+        }
+
+        [HttpPost("")]
+        public IActionResult Insere([FromBody] SerieModel model)
+        {
+            Serie serie = model.ToSerie();
+
+            serie.ID = _repositorioSerie.ProximoID();
+
+            _repositorioSerie.Insere(serie);
+            return Created("", serie);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Consulta(int id)
+        {
+            return Ok(_repositorioSerie.Lista().FirstOrDefault(s => s.ID == id));
+        }
+
+    }
+}
---
+++ DIO.Series.Web/SerieModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Web
+{
+    public class SerieModel
+    {
+        public int Id { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public bool Excluido { get; set; }
+
+        public SerieModel(Serie serie)
+        {
+            Id = serie.ID;
+            Genero = serie.Genero;
+            Titulo = serie.Titulo;
+            Descricao = serie.Descricao;
+            Ano = serie.Ano;
+            Excluido = serie.Excluido;
+        }
+
+        public SerieModel()
+        {
+
+        }
+
+        public Serie ToSerie()
+        {
+            return new Serie(Id, Genero, Titulo, Descricao, Ano);
+        }
+    }
+}
